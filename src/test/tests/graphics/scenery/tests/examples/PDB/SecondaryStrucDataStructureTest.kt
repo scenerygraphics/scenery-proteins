59b626df7333b335e16bed0446ee46bb73a570d2
Domino2357 <JustinBuerger@googlemail.com>
2020-01-22 16:02:34 +0100

    Tested Secondary Structure DataStructure

--- src/test/tests/graphics/scenery/tests/examples/PDB/SecondaryStrucDataStructureTest.kt
+++ src/test/tests/graphics/scenery/tests/examples/PDB/SecondaryStrucDataStructureTest.kt
@@ -1,2 +1,17 @@
 package graphics.scenery.tests.examples.PDB
 
+import graphics.scenery.Protein
+import graphics.scenery.proteins.SecondaryStructure
+import org.junit.Test
+
+class SecondaryStructureDataStructureTest {
+
+    @Test
+    fun main() {
+        val protein = Protein.fromID("3nir")
+        val ss = SecondaryStructure(protein)
+        ss.secondaryStruc().forEach{
+            print(it.type)
+        }
+    }
+}
--- src/test/tests/graphics/scenery/tests/examples/PDB/SecondaryStructureCalcTest.kt
+++ src/test/tests/graphics/scenery/tests/examples/PDB/SecondaryStructureCalcTest.kt
@@ -1,5 +1,6 @@
 package graphics.scenery.tests.examples.PDB
 
+import graphics.scenery.Protein
 import graphics.scenery.proteins.AminoList
 import graphics.scenery.proteins.SecondaryStructure
 import org.junit.Test
@@ -9,7 +10,8 @@ class SecondaryStructureCalcTest {
 
     @Test
     fun main() {
-        val ss = SecondaryStructure("3nir")
+        val p = Protein.fromID("3nir")
+        val ss = SecondaryStructure(p)
         ss.secondaryStruc()
     }
 }
