1015bb7b5ec2c54fb831dabe2504d2e4fd745f30
Domino2357 <JustinBuerger@googlemail.com>
2020-02-11 14:32:37 +0100

    Changed the logic of the curve geometry class

--- src/main/kotlin/graphics/scenery/proteins/CurveGeometry.kt
+++ src/main/kotlin/graphics/scenery/proteins/CurveGeometry.kt
@@ -1,19 +1,30 @@
 package graphics.scenery.proteins
 
 import cleargl.GLVector
-import graphics.scenery.HasGeometry
-import graphics.scenery.Mesh
+import graphics.scenery.*
+import org.lwjgl.opengl.GL
+import java.nio.FloatBuffer
+import java.nio.IntBuffer
 
-class CurveGeometry(func: (List<GLVector>, Any) -> (List<GLVector>)): Mesh("CurveGeometry"), HasGeometry {
+class CurveGeometry(val curve: List<GLVector>): Node("CurveGeometry"), HasGeometry {
+    override val vertexSize = 3
+    override val texcoordSize = 2
+    override var geometryType = GeometryType.TRIANGLES
 
-    //Creates a point in 2D with an Index and x,y coordinates
-    data class IndexedPoint(val values: Triple<Int, Float, Float>)
+    override var vertices: FloatBuffer = BufferUtils.allocateFloat(0)
+    override var normals: FloatBuffer = BufferUtils.allocateFloat(0)
+    override var texcoords: FloatBuffer = BufferUtils.allocateFloat(0)
+    override var indices: IntBuffer = BufferUtils.allocateInt(0)
 
-    //This resembles a closed Graph in 2D it contains points with an index
-    data class Closed2DGraph(val points: List<IndexedPoint>)
-
-
-    fun calcWith2DGraph(curve: List<GLVector>, graph: Closed2DGraph) {
-
+    fun drawSpline(baseShape: (() -> List<GLVector>)) {
+        curve.forEach {
+            val p = it
+            baseShape.invoke().forEach{
+                /*Todo! here the vertices need to be added. You have to implement a frenet frame which
+                  Todo! rides along the curve. Use this frame to calculate the coordinates for your base
+                  Todo! shape at a given point in the curve. For all these new points find an algorithm
+                  Todo! which orders and stores them as triangles.  */
+            }
+        }
     }
 }
