4da5bb91350af6812c008053f245d7d3372b9a99
Domino2357 <JustinBuerger@googlemail.com>
2020-03-27 10:12:26 +0100

    Wrote BSPlineTest

--- src/main/kotlin/graphics/scenery/proteins/UniformBSpline.kt
+++ src/main/kotlin/graphics/scenery/proteins/UniformBSpline.kt
@@ -8,43 +8,44 @@ import cleargl.GLVector
  * does not necessarily pass through the control points. If you need a curve, which actually contains the
  * control points, use the Catmull Rom Spline instead. However, if this is not required and you prefer a
  * smoother curve over the exact one, this is the spline to draw.
- * The control points are represented by a List of Vectors ([controlpoints]) and the number of points the
+ * The control points are represented by a List of Vectors ([controlPoints]) and the number of points the
  * curve shall contain is [n], which is the hundred times the number of controlpoints by default.
  */
-class UniformBSpline(val controlpoints: ArrayList<GLVector>, val n: Int = controlpoints.size*100) {
+class UniformBSpline(val controlPoints: ArrayList<GLVector>, val n: Int = controlPoints.size*100) {
 
-    val basisMatrix = GLMatrix(floatArrayOf(
-            0f, 0f, 0f, 1/6f,
-            1/6f, 1/2f, 1/2f, -1/2f,
-            2/3f, 0f, -1f, 1/2f,
-            1/6f, -1/2f, 1/2f, -1/6f
-    ))
+    private val array = floatArrayOf(
+            0f, 0f, 0f, 1f,
+            1f, 3f, 3f, -3f,
+            4f, 0f, -6f, 3f,
+            1f, -3f, 3f, -1f)
+    private val basisMatrix = GLMatrix(array)
 
-    fun partialSpline(p1: GLVector, p2: GLVector, p3: GLVector, p4: GLVector): ArrayList<GLVector> {
+    private fun partialSpline(p1: GLVector, p2: GLVector, p3: GLVector, p4: GLVector, j: Int): ArrayList<GLVector> {
         val pointMatrix = GLMatrix(floatArrayOf(
                 p1.x(), p2.x(), p3.x(), p4.x(),
                 p1.y(), p2.y(), p3.y(), p4.y(),
                 p1.z(), p2.z(), p3.z(), p4.z(),
                 1f, 1f, 1f, 1f))
-        val intermediateMatrix = GLMatrix(pointMatrix.mult(basisMatrix.floatArray))
+        pointMatrix.mult(basisMatrix)
         val partialSpline = ArrayList<GLVector>(n)
 
         for(i in 1..n) {
-            val t = (i/n).toFloat()
+            val t = i/n.toFloat()
             val tVector = GLVector(1f, t, t*t, t*t*t)
-            partialSpline.add(intermediateMatrix.mult(tVector.xyz()))
+            partialSpline.add(pointMatrix.mult(tVector).times(1/6f).xyz())
         }
 
         return(partialSpline)
     }
 
     fun bSplineCurvePoints(): ArrayList<GLVector> {
-        val curvepoints = ArrayList<GLVector>((controlpoints.size-3)*n +1)
-        controlpoints.dropLast(3).forEachIndexed{ index, _ ->
-            val spline = partialSpline(controlpoints[index], controlpoints[index +1],
-            controlpoints[index+2], controlpoints[index+3])
+        val curvepoints = ArrayList<GLVector>((controlPoints.size-3)*n +1)
+        controlPoints.dropLast(3).forEachIndexed{ index, _ ->
+            val spline = partialSpline(controlPoints[index], controlPoints[index +1],
+            controlPoints[index+2], controlPoints[index+3])
             curvepoints.addAll(spline)
         }
+        println(curvepoints)
         return curvepoints
     }
 }
