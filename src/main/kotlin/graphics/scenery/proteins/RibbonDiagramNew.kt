2adb93f4ce58ef58aee41fa34065ec28797215d0
Domino2357 <JustinBuerger@googlemail.com>
2020-03-30 19:48:53 +0200

    Added the guide points for the backbone and an, hopefully, exhaustive TODO.

--- src/main/kotlin/graphics/scenery/proteins/RibbonDiagramNew.kt
+++ src/main/kotlin/graphics/scenery/proteins/RibbonDiagramNew.kt
@@ -3,9 +3,12 @@ package graphics.scenery.proteins
 import cleargl.GLVector
 import graphics.scenery.Mesh
 import graphics.scenery.Protein
+import graphics.scenery.numerics.Random.Companion.randomFromRange
 import org.biojava.nbio.structure.AminoAcid
 import org.biojava.nbio.structure.Group
 import org.biojava.nbio.structure.HetatomImpl
+import java.lang.Float.min
+import kotlin.random.Random
 
 /**
  * A Ribbon Diagram for Proteins. Ribbon models are currently the most common representations of proteins.
@@ -47,18 +50,106 @@ class RibbonDiagramNew(val protein: Protein): Mesh("RibbonDiagram") {
 
     data class GuidePoint(val finalPoint: GLVector, val cVec: GLVector, val dVec: GLVector,
                           val offset: Float = 0f, val widthFactor: Float = 0f,
-                          val prevResidue: Group, val nextResidue: Group)
+                          val prevResidue: Group?, val nextResidue: Group?)
 
-    fun calculateGuidePoints(): ArrayList<GuidePoint> {
-        val guidePoints = ArrayList<GuidePoint>(groups.size)
+    fun calculateGuidePoints(aminoList: List<Group>): ArrayList<GuidePoint> {
+        val aminoList = aminoList.filter { it.isAminoAcid && it.hasAtom("CA") && it.hasAtom("O")}
+        val guidePoints = ArrayList<GuidePoint>(aminoList.size+4-1)
+        val maxOffSet = 1.5f
+        aminoList.dropLast(2).forEachIndexed{ i, _ ->
+            val ca1 = aminoList[i].getAtom("CA")
+            val ca2 = aminoList[i+1].getAtom("CA")
+            val ca1ca2 = GLVector((ca1.x.toFloat()-ca2.x.toFloat()),
+                    (ca1.y.toFloat()-ca2.y.toFloat()),
+                    (ca1.z.toFloat()-ca2.z.toFloat()))
+            val o = aminoList[i].getAtom("O")
+            val oxygenVec = GLVector(o.x.toFloat(), o.y.toFloat(), o.z.toFloat())
+
+            val finalPoint = ca1ca2.times(0.5f)
+            //See Carlson et. al
+            val cVec = ca1ca2.cross(oxygenVec).normalized
+            val dVec = cVec.cross(cVec).normalized
+            val widthFactor: Float
+            val offset: Float
+            /*
+             "Based on Ca(i-1) to Ca(i+2) distance, we may adjust ribbon width
+             and/or guidepoint position. Ribbon widens in areas of high OR low
+             curvature (alpha/beta, respectively). This is only a preliminary
+             estimate -- it's applied only for 3+ residues in a row. (checked below)
+
+             For high curvature ONLY (alpha), we offset the guidepoint
+             outwards to make the spline track the chain (esp. helix);
+             this is done for each and does not require 3+ in a row.
+             Offset vector goes from the midpoint of Ca(i-1) to Ca(i+2)
+             thru the current guide point
+             (which is the midpoint of Ca(i) and Ca(i+1)).
+
+             CA-CA DIST  WIDTH FACTOR    OFFSET FACTOR   NOTE
+                  ==========  ============    =============   ====
+                  5.0         1               1               ~limit for curled-up protein
+                  5.5         1               1               } linear interpolation
+                  7.0         0               0               } from 1.0 to 0.0
+                  9.0         0               0               } linear interpolation
+                  10.5        1               0               } from 1.0 to 0.0
+                  11.0        1               0               ~limit for extended protein",
+                  from: KiNG
+             */
+            if(i >= 1 && i < aminoList.size-3) {
+                val ca0 = aminoList[i-1].getAtom("CA")
+                val ca3 = aminoList[i+2].getAtom("CA")
+                val ca0Ca3 = GLVector((ca0.x.toFloat()- ca3.x.toFloat()),
+                        (ca0.y.toFloat()- ca3.y.toFloat()),
+                        (ca0.z.toFloat()- ca3.z.toFloat()))
+                val ca0Ca3Distance = ca0Ca3.length2()
+                when {
+                    (ca0Ca3Distance> 7f) -> {
+                        widthFactor = min(1.5f, 7f - ca0Ca3Distance) / 1.5f
+                        offset = min(1.5f, ca0Ca3Distance - 9f) / 1.5f
+                        val ca0Ca3Midpoint = ca0Ca3.times(0.5f)
+                        val offsetVec = finalPoint.minus(ca0Ca3Midpoint).normalized.times(offset*maxOffSet)
+                        finalPoint.plus(offsetVec)
+                    }
+                    (ca0Ca3Distance < 9f) -> {
+                        widthFactor = min(1.5f, ca0Ca3Distance - 9f) / 1.5f
+                        offset = 0f
+                    }
+                    else -> {
+                        widthFactor = 0f
+                        offset = 0f
+                    }
+                }
+                guidePoints.add(i+2, GuidePoint(finalPoint, cVec, dVec, offset, widthFactor,
+                aminoList[i-1], aminoList[i+1]))
+            }
+            else {
+                widthFactor = 0f
+                offset = 0f
+                guidePoints.add(i+2, GuidePoint(finalPoint, cVec, dVec, offset, widthFactor,
+                        null, null))
+            }
+
+        }
+        //TODO see why they are using [i+2] for the guide point and [i+1] for its next residue,
+        //TODO this doesn't make much sense to me. Also they are making two dummy vectors at the
+        //TODO beginning and the end, which would only make sense if they want to assign the
+        //TODO correct width factor to each residue, but this comes with a price: the control points
+        //TODO of the first residue are not calculated exactly. But on a second thought this is probably
+        //TODO the way to go since it would be impossible to change the offset afterwards (the first
+        //TODO residue has no predecessor and the last one no successor). But the problem remains, why
+        //TODO they are using the direct predecessor of a residue and name it "nextRes". This is the puzzle
+        //TODO to solve.
+        //TODO Besides, this function should do it. Then you need to find out, whether there are different chains
+        //TODO in a protein, well of course there are but is it possible to have more than one c aplha trace?
+        //TODO Afterwards, assign each Residue a secondary Structure (ss) if there are more than three in a row and
+        //TODO the offset is bigger than 0, draw the ss with a Bspline with the control points from predecessor
+        //TODO of the beginning of the ss to the successor of the end of ss. This should do it and the only
+        //TODO open question remains: How to elegantly draw the different geometries?
+        return guidePoints
+        /*
         val allAminoAcids = chains.filter{it.isProtein}.flatMap{ chain ->
             chain.atomGroups.filter{it.isAminoAcid && it.hasAtom("CA") }}
         val allAminoAcidsDistinct = distinctChains(allAminoAcids)
-        //TODO look at the Ribbons Class to see exactly what they are doing with
-        //TODO the distinct chains (do they draw them each or only the longest chain
-        //TODO or what not? Then create the guidePoints according to the code and finally
-        //TODO see what you can do to assign secondary structures effectively. :)
-        return guidePoints
+         */
     }
 
     private fun distinctChains(aminoAcids: List<Group>): ArrayList<ArrayList<Group>> {
