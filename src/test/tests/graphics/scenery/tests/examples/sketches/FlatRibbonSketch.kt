e1391e646092b31e99aea24cc6c4dfca63612095
Domino2357 <JustinBuerger@googlemail.com>
2020-08-20 17:35:38 +0200

    Renamed RibbonVisualization to RibbonDiagram; made guidePointcalculation a companion object.

--- src/test/tests/graphics/scenery/tests/examples/sketches/FlatRibbonSketch.kt
+++ src/test/tests/graphics/scenery/tests/examples/sketches/FlatRibbonSketch.kt
@@ -4,7 +4,7 @@ import org.joml.*
 import graphics.scenery.*
 import graphics.scenery.backends.Renderer
 import graphics.scenery.numerics.Random
-import graphics.scenery.proteins.RibbonVisualization
+import graphics.scenery.proteins.RibbonDiagram
 import org.junit.Test
 
 class FlatRibbonSketch: SceneryBase("FlatRibbonSketch", windowWidth = 1280, windowHeight = 720) {
@@ -16,7 +16,7 @@ class FlatRibbonSketch: SceneryBase("FlatRibbonSketch", windowWidth = 1280, wind
 
         val protein = Protein.fromID("2w49")
 
-        val diagram = RibbonVisualization(protein)
+        val diagram = RibbonDiagram(protein)
 
         val curve = diagram.ribbonCurve()
 
--- src/test/tests/unit/RibbonDiagramTests.kt
+++ src/test/tests/unit/RibbonDiagramTests.kt
@@ -1,7 +1,7 @@
 package unit
 
 import graphics.scenery.Protein
-import graphics.scenery.proteins.RibbonVisualization
+import graphics.scenery.proteins.RibbonDiagram
 import graphics.scenery.utils.LazyLogger
 import org.junit.Test
 import kotlin.test.assertNotNull
@@ -9,7 +9,7 @@ import kotlin.test.assertNotNull
 /**
  * This is the test for the RibbonCalculation, i.e. the pdb-support.
  */
-class RibbonVisualizationTests {
+class RibbonDiagramTests {
     private val logger by LazyLogger()
 
     /**
@@ -19,7 +19,7 @@ class RibbonVisualizationTests {
     fun prionTest() {
         logger.info("Tests coherence of curve size and number of residues.")
         val prion = Protein.fromID("2rnm")
-        val ribbon = RibbonVisualization(prion)
+        val ribbon = RibbonDiagram(prion)
         val curve = ribbon.ribbonCurve()
         assertNotNull(curve)
     }
