d8c73a5ef8a21ca6b078716a237e59027f0bfdfd
Domino2357 <JustinBuerger@googlemail.com>
2020-09-23 14:55:47 +0200

    RibbonDiagramTests: corrected numberSubProteinsTest

--- src/test/tests/unit/RibbonDiagramTests.kt
+++ src/test/tests/unit/RibbonDiagramTests.kt
@@ -7,6 +7,7 @@ import org.biojava.nbio.structure.Group
 import org.junit.Test
 import kotlin.reflect.full.declaredMemberFunctions
 import kotlin.reflect.jvm.isAccessible
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
@@ -38,27 +39,27 @@ class RibbonDiagramTests {
         logger.info("Tests number of subProteins.")
         val plantProtein = Protein.fromID("3nir")
         val plantRibbon = RibbonDiagram(plantProtein)
-        assertTrue { plantRibbon.children.size == 1 }
+        assertEquals (plantRibbon.children.size, 1)
 
         val insectWing = Protein.fromID("2w49")
         val insectWingRibbon = RibbonDiagram(insectWing)
-        assertTrue { insectWingRibbon.children.size == 5 }
+        assertEquals (insectWingRibbon.children.size, 36)
 
         val saccharomycesCerevisiae = Protein.fromID("6zqd")
         val scRibbon = RibbonDiagram(saccharomycesCerevisiae)
-        assertTrue { scRibbon.children.size == 60 }
+        assertEquals( scRibbon.children.size , 63)
 
         val covid19 = Protein.fromID("6zcz")
         val covidRibbon = RibbonDiagram(covid19)
-        assertTrue { covidRibbon.children.size == 4 }
+        assertEquals( covidRibbon.children.size , 4)
 
         val aspirin = Protein.fromID("6mqf")
         val aspirinRibbon = RibbonDiagram(aspirin)
-        assertTrue { aspirinRibbon.children.size == 1 }
+        assertEquals (aspirinRibbon.children.size, 2)
 
         val nucleosome = Protein.fromID("6y5e")
         val nucRibbon = RibbonDiagram(nucleosome)
-        assertTrue{ nucRibbon.children.size == 2 }
+        assertEquals (nucRibbon.children.size, 9)
 
     }
 
