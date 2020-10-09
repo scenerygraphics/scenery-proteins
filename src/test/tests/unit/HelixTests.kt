968d65f68ab4a178d42f4e7b12215b6c0f0208a1
Domino2357 <JustinBuerger@googlemail.com>
2020-10-09 16:46:24 +0200

    HelixTests: set up test class

---
+++ src/test/tests/unit/HelixTests.kt
@@ -0,0 +1,14 @@
+package unit
+
+import graphics.scenery.utils.LazyLogger
+
+/**
+ * This is the test class for the Helix class.
+ */
+
+class HelixTests {
+
+    private val logger by LazyLogger()
+
+
+}
