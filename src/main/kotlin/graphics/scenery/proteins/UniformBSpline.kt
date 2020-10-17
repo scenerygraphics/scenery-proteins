39edc3f3ac342ae91597b83c0e22f91399b0e369
Domino2357 <JustinBuerger@googlemail.com>
2020-10-17 09:00:58 +0200

    UniformBSpline: made properties private

--- src/main/kotlin/graphics/scenery/proteins/UniformBSpline.kt
+++ src/main/kotlin/graphics/scenery/proteins/UniformBSpline.kt
@@ -19,7 +19,7 @@ import java.lang.IllegalArgumentException
  *
  * @author Justin Bürger <burger@mpi-cbg.de>
  */
-class UniformBSpline(protected val controlPoints: ArrayList<Vector3f>, val n: Int = 100): Spline {
+class UniformBSpline(private val controlPoints: ArrayList<Vector3f>, private val n: Int = 100): Spline {
 
     /**
      * This is a list of the equidistant parameters at which the curve is calculated.
