ed431d79a89f04e32dae460ddd3b576aef0e8d90
Domino2357 <JustinBuerger@googlemail.com>
2020-10-19 18:08:56 +0200

    Added docu and cleaned up the class.

--- src/main/kotlin/graphics/scenery/proteins/Rainbow.kt
+++ src/main/kotlin/graphics/scenery/proteins/Rainbow.kt
@@ -3,9 +3,31 @@ package graphics.scenery.proteins
 import graphics.scenery.Node
 import org.joml.Vector3f
 
+/**
+ * This is the class which stores the calculation for a color vector along a Mesh.
+ *
+ */
+//TODO implement iteration depth that the number of children is flexible
 class Rainbow {
+    /*
+         Palette Rainbow colors palette has 7 HEX, RGB codes colors:
+         HEX: #ff0000 RGB: (255, 0, 0),
+         HEX: #ffa500 RGB: (255, 165, 0),
+         HEX: #ffff00 RGB: (255, 255, 0),
+         HEX: #008000 RGB: (0, 128, 0),
+         HEX: #0000ff RGB: (0, 0, 255),
+         HEX: #4b0082 RGB: (75, 0, 130),
+         HEX: #ee82ee RGB: (238, 130, 238).*
+
+         *see: https://colorswall.com/palette/102/
+         */
+    private val rainbowPaletteNotScaled = listOf(Vector3f(255f, 0f, 0f), Vector3f(255f, 165f, 0f),
+            Vector3f(255f, 255f, 0f), Vector3f(0f, 128f, 0f), Vector3f(0f, 0f, 255f),
+            Vector3f(75f, 0f, 135f), Vector3f(238f, 130f, 238f))
+    private val rainbowPalette = rainbowPaletteNotScaled.map { it.mul(1/255f) }
+
     /**
-     * Assigns each residue its color- if displaySS is false.
+     * Assigns each child its color.
      */
     fun colorVector(subProtein: Node) {
         var childrenSize = 0
@@ -18,22 +40,6 @@ class Rainbow {
         }
 
         val childrenCount = childrenSize
-        /*
-         Palette Rainbow colors palette has 7 HEX, RGB codes colors:
-         HEX: #ff0000 RGB: (255, 0, 0),
-         HEX: #ffa500 RGB: (255, 165, 0),
-         HEX: #ffff00 RGB: (255, 255, 0),
-         HEX: #008000 RGB: (0, 128, 0),
-         HEX: #0000ff RGB: (0, 0, 255),
-         HEX: #4b0082 RGB: (75, 0, 130),
-         HEX: #ee82ee RGB: (238, 130, 238).*
-
-         *see: https://colorswall.com/palette/102/
-         */
-        val rainbowPaletteNotScaled = listOf(Vector3f(255f, 0f, 0f), Vector3f(255f, 165f, 0f),
-                Vector3f(255f, 255f, 0f), Vector3f(0f, 128f, 0f), Vector3f(0f, 0f, 255f),
-                Vector3f(75f, 0f, 135f), Vector3f(238f, 130f, 238f))
-        val rainbowPalette = rainbowPaletteNotScaled.map { it.mul(1/255f) }
         val sixth = (childrenCount/6)
         val colorList = ArrayList<Vector3f>(childrenCount)
         for(j in 1..6) {
