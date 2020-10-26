7dbf031841496e576f1097eedd666e8c6b0c07f8
Domino2357 <JustinBuerger@googlemail.com>
2020-10-26 15:48:37 +0100

    Customized CreateCommand

--- src/main/kotlin/graphics/scenery/proteins/ruler/CreateCommand.kt
+++ src/main/kotlin/graphics/scenery/proteins/ruler/CreateCommand.kt
@@ -5,6 +5,7 @@ import graphics.scenery.Mesh
 import graphics.scenery.Scene
 import graphics.scenery.backends.Renderer
 import graphics.scenery.utils.LazyLogger
+import org.joml.Vector2f
 import org.joml.Vector3f
 import org.scijava.ui.behaviour.ClickBehaviour
 import kotlin.reflect.KProperty
@@ -16,10 +17,12 @@ open class CreateCommand @JvmOverloads constructor(protected val name: String,
                                                    protected val renderer: Renderer,
                                                    protected val scene: Scene,
                                                    protected val camera: () -> Camera?,
-                                                    protected val mesh: Mesh) : ClickBehaviour {
+                                                    protected val meshLambda: () -> Mesh) : ClickBehaviour {
     protected val logger by LazyLogger()
 
     protected val cam: Camera? by CameraDelegate()
+
+    protected val mesh = meshLambda.invoke()
 
     /** Camera delegate class, converting lambdas to Cameras. */
     protected inner class CameraDelegate {
@@ -42,7 +45,11 @@ open class CreateCommand @JvmOverloads constructor(protected val name: String,
     override fun click(x: Int, y: Int) {
         scene.updateWorld(true)
         mesh.parent = scene
-        mesh.position = Vector3f()
+        val position = Vector3f()
+        position.set(cam!!.position)
+        val z = Vector3f()
+        z.set(cam!!.cameraTripod().z)
+        mesh.position = position.add(z.normalize().mul(5f))
         scene.addChild(mesh)
     }
 }
