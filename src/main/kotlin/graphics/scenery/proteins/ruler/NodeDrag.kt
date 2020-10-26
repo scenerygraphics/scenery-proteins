c594d593884245ee6ffd7f456031f1402fd0e59e
Domino2357 <JustinBuerger@googlemail.com>
2020-10-26 17:16:13 +0100

    NodeDrag: setup the class

---
+++ src/main/kotlin/graphics/scenery/proteins/ruler/NodeDrag.kt
@@ -0,0 +1,19 @@
+package graphics.scenery.proteins.ruler
+
+import graphics.scenery.Camera
+import graphics.scenery.Node
+import org.scijava.ui.behaviour.DragBehaviour
+
+class NodeDrag(private val name: String, private val cam: Camera, private val node: Node): DragBehaviour {
+    override fun init(p0: Int, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun drag(p0: Int, p1: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun end(p0: Int, p1: Int) {
+        TODO("Not yet implemented")
+    }
+}
