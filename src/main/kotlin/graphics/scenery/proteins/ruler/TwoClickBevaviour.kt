2c47cd3bab0895c83ef32300de8daed344db8a83
Domino2357 <JustinBuerger@googlemail.com>
2020-11-10 20:01:19 +0100

    TwoClickBehaviour: deleted because it (probably) deos not work.

--- src/main/kotlin/graphics/scenery/proteins/ruler/TwoClickBevaviour.kt
+++
@@ -1,8 +0,0 @@
-package graphics.scenery.proteins.ruler
-
-import org.scijava.ui.behaviour.Behaviour
-
-interface TwoClickBevaviour: Behaviour {
-    fun firstClick(x: Int, y: Int)
-    fun secondClick(x: Int, y: Int)
-}
