1bc0ca672b50c590717d2c839ae7ba519cf52781
Domino2357 <JustinBuerger@googlemail.com>
2020-11-10 20:04:07 +0100

    SetUp CreateCommandRayCast.

---
+++ src/main/kotlin/graphics/scenery/proteins/ruler/CreateCommandRayCast.kt
@@ -0,0 +1,9 @@
+package graphics.scenery.proteins.ruler
+
+import org.scijava.ui.behaviour.ClickBehaviour
+
+class CreateCommandRayCast: ClickBehaviour {
+    override fun click(p0: Int, p1: Int) {
+        TODO("Not yet implemented")
+    }
+}
