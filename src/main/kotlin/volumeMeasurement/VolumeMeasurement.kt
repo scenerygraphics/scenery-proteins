8a353fa9752b7b422ab5cb94e254b872d2ebe66d
Domino2357 <JustinBuerger@googlemail.com>
2020-11-13 14:57:13 +0100

    Fixed VolumeMeasurement.

--- src/main/kotlin/volumeMeasurement/VolumeMeasurement.kt
+++ src/main/kotlin/volumeMeasurement/VolumeMeasurement.kt
@@ -1,34 +1,39 @@
 package volumeMeasurement
 
+import graphics.scenery.GeometryType
 import graphics.scenery.Mesh
+import kotlin.math.absoluteValue
 
 class VolumeMeasurement {
-    fun calculateVolume(mesh: Mesh): Float{
+    fun calculateVolume(mesh: Mesh): Double {
         val vertices = mesh.vertices
-        if(vertices.hasArray()) {
-            val verticesAsList = vertices.array()
-            val arraySize = verticesAsList.size
-            val numberOfSubVolumes = arraySize % 9
-            val subVolumes = ArrayList<Float>(numberOfSubVolumes)
-            if (arraySize % 9 == 0) {
-                var i = 0
-                while (i < arraySize) {
-                    val x1 = verticesAsList[i]
-                    val y1 = verticesAsList[i + 1]
-                    val z1 = verticesAsList[i + 2]
-                    val x2 = verticesAsList[i + 3]
-                    val y2 = verticesAsList[i + 4]
-                    val z2 = verticesAsList[i + 5]
-                    val x3 = verticesAsList[i + 6]
-                    val y3 = verticesAsList[i + 7]
-                    val z3 = verticesAsList[i + 8]
-                    val volume = 1 / 6 * (-x3 * y2 * z1 + x2 * y3 * z1 + x3 * y1 * z2 - x1 * y3 * z2 - x2 * y1 * z3 + x1 * y2 * z3)
-                    subVolumes.add(volume)
-                    i += 9
-                }
+        val vertexBuffer = vertices.asReadOnlyBuffer()
+        val arraySize = vertexBuffer.limit()
+        val numberOfSubVolumes = arraySize%9
+        val subVolumes = ArrayList<Float>(numberOfSubVolumes)
+        if(arraySize%9 == 0 && mesh.geometryType == GeometryType.TRIANGLES) {
+            var i = 0
+            while (i < arraySize) {
+                val x1 = vertexBuffer.get(i)
+                val y1 = vertexBuffer.get(i+1)
+                val z1 = vertexBuffer.get(i+2)
+                val x3 = vertexBuffer.get(i+3)
+                val y3 = vertexBuffer.get(i+4)
+                val z3 = vertexBuffer.get(i+5)
+                val x2 = vertexBuffer.get(i+6)
+                val y2 = vertexBuffer.get(i+7)
+                val z2 = vertexBuffer.get(i+8)
+                val i1 = (x3 * y2 * z1)
+                val i2 = (x2 * y3 * z1)
+                val i3 = (x3 * y1 * z2)
+                val i4 = (x1 * y3 * z2)
+                val i5 = (x2 * y1 * z3)
+                val i6 = (x1 * y2 * z3)
+                val volume = (-i1 + i2 + i3 - i4 - i5 + i6)/6f
+                subVolumes.add(volume)
+                i += 9
             }
-            return (subVolumes.fold(0f) { acc, next -> acc + next })
         }
-        else { return 0f }
+        return (subVolumes.fold(0.0) {acc, next -> acc + next }.absoluteValue)
     }
 }
